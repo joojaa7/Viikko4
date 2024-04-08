9fa2ac4a885b26f300b174fc96001c4c82e055f7
joojaa7 <Mikael.Asikainen@gmail.com>
2024-04-08 14:46:21 +0300

    as2

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import api from './api/index.js';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api/v1', api);
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+export default app;
